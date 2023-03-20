62d629dc31bb56fd581f29d0ab50c0330ff0e9bd
vm-phung1134 <phucnguyen1134@gmail.com>
2023-03-20 22:05:31 +0700

    update README.md

--- back-end/routes/ticketRoute.js
+++ back-end/routes/ticketRoute.js
@@ -2,7 +2,6 @@ const express = require('express')
 const Router = express.Router()
 const {getAllTickets, createTicket, updateTicket, deleteTicket}
     = require('../controllers/ticketController')
-const {verifyToken} = require('../middleware/verifyToken')
 
 Router.route('/').get(getAllTickets).post(createTicket)
 Router.route('/:ticketId').put(updateTicket).delete(deleteTicket)
